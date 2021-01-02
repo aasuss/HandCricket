12c93aff832332128bfdd5a372f8acd1ad6bcef5
AsHiSh-N <ashish nehra8166@gmail.com>
2021-01-02 20:27:39 +0530

    scorecard update

--- cricket.js
+++ cricket.js
@@ -1,51 +1,82 @@
-class GameState{
-    constructor(_totalOvers){
-        this.turn = 1;
-        this.innings = 1;
-        this.over = 1;
-        this.ball = 1;
-        this.totalOvers =_totalOvers;
+class GameState {
+  constructor() {
+    this.turn = null;
+    this.ball = 1;
+    this.myTurn = 1;
+    this.totalWickets = 3;
 
+    this.player1 = {};
+    this.player1.currentMove = 0;
+    this.player1.runs = 0;
+    this.player1.wickets = 0;
 
-        let player;
-        player.currentMove = "";
-        player.runs = 0;
-        player.wickets = 0;
-        this.player1 = player;
-        this.player2 = player;
-    }
-    makeMove(move){
-        if(this.player2.currentMove){
-            // send my move and show all moves
-            this.changeBall();
-            
-        }
-        else{
-            this.player1.currentMove = move;
-        }
-
-    }
-    recieveMove(move){
-
-        if(this.player1.currentMove){
-            // show all moves
-            this.changeBall();
-            
-        }
-        else{
-            // store their move
-        }
-
-    }
-    changeBall(){
-        if(this.ball == 6){
-            this.ball = 1;
-            this.over = this.over+1; 
-        }
-        this.player1.currentMove = null;
-        this.player2.currentMove = null;
-
-    }
-
+    this.player2 = {};
+    this.player2.currentMove = 0;
+    this.player2.runs = 0;
+    this.player2.wickets = 0;
     
+  }
+
+  makeMove(move) {
+    if(!this.player1.currentMove){
+      this.player1.currentMove = move;
+      document.getElementById("player-hand").src = "/images/" + move + ".svg";
+      gameConn.sendData(JSON.stringify(move));
+      
+      if (this.player2.currentMove) {
+        //show all moves
+        document.getElementById("opponent-hand").src = "/images/" + this.player2.currentMove + ".svg";
+        this.evaluateBall();
+        this.changeBall();
+      } 
+
+      
+    }
+    
+  }
+
+  recieveMove(move) {
+    if (this.player1.currentMove) {
+      // show all moves
+      document.getElementById("opponent-hand").src = "/images/" + move + ".svg";
+      this.evaluateBall();
+      this.changeBall();
+    } else {
+      this.player2.currentMove = move;
+    }
+  }
+
+
+
+  setTurn(turn) {
+    this.turn = turn;
+  }
+
+  changeBall() {
+    this.ball = this.ball + 1;
+
+    this.player1.currentMove = 0;
+    this.player2.currentMove = 0;
+    
+  }
+  
+  evaluateBall(){
+    this.ball += 1; 
+    if(this.player1.currentMove == this.player2.currentMove){
+      this[this.turn].wickets += 1 ; 
+    }
+    if(this.player1.currentMove != this.player2.currentMove){
+      this[this.turn].runs += this[this.turn].currentMove;
+    }
+    
+    setTimeout(function (){
+      document.getElementById("player-hand").src = "/images/7.svg";
+      document.getElementById("opponent-hand").src = "/images/7.svg";
+      document.getElementById("current-score").innerHTML = this.player1.runs + " - " + this.player1.wickets ;
+      document.getElementById("last-inning-score").innerHTML = this.player2.runs + " - " + this.player2.wickets ;
+    }.bind(this),2000)
+    console.log(this)
+  }
 }
+
+
--- script.js
+++ script.js
@@ -1,15 +1,9 @@
+var gameConn = null;
 window.onload = ticTacToe;
+
 function ticTacToe() {
   // Connection
-  var gameConn = null;
   var gamecodeDiv = document.getElementById("gamecode");
-  var runs1 = 0;
-  var runs2 = 0;
-  var wickets1 = 0;
-  var wickets2 = 0;
-  var gameState = {};
-  gameState.player1;
-
   var modal = document.getElementById("myModal");
   var usernameInput = document.getElementById("username");
   var gameroomInput = document.getElementById("gameroom");
@@ -18,74 +12,26 @@ function ticTacToe() {
   var joingameBtn = document.getElementById("joinGame");
   modal.style.display = "block";
 
-  // Get the modal
-  var modal1 = document.getElementById("myModal1");
-  var btn = document.getElementById("myBtnn");
-  var span = document.getElementsByClassName("close1")[0];
-  btn.onclick = function () {
-    modal1.style.display = "block";
-  };
-
-  span.onclick = function () {
-    modal1.style.display = "none";
-  };
-  window.onclick = function (event) {
-    if (event.target == modal1) {
-      modal1.style.display = "none";
-    }
-  };
-
-  const coin = document.querySelector("#coin");
-  const button = document.querySelector("#flip");
-  const status = document.querySelector("#status");
-  const heads = document.querySelector("#headsCount");
-  const tails = document.querySelector("#tailsCount");
-
-  let headsCount = 0;
-  let tailsCount = 0;
-
-  function deferFn(callback, ms) {
-    setTimeout(callback, ms);
-  }
-
-  function processResult(result) {
-    if (result === "heads") {
-      headsCount++;
-      heads.innerText = headsCount;
-    } else {
-      tailsCount++;
-      tails.innerText = tailsCount;
-    }
-    status.innerText = result.toUpperCase();
-  }
-
-  function flipCoin() {
-    coin.setAttribute("class", "");
-    const random = Math.random();
-    const result = random < 0.5 ? "heads" : "tails";
-    deferFn(function () {
-      coin.setAttribute("class", "animate-" + result);
-      deferFn(processResult.bind(null, result), 2900);
-    }, 100);
-  }
-
-  button.addEventListener("click", flipCoin);
+  // Game State Initialization
+  let gameState = new GameState();
 
   function onGetData(data) {
     if (data.username) {
       modal.style.display = "none";
     } else {
-      var num = document.getElementById(data);
-      leftHand(data);
+      // leftHand(data);
+      gameState.recieveMove(data);
+
     }
 
     console.log(data, " Log by Callback");
   }
-  var n;
+
   function leftHand(n) {
     document.getElementById("opponent-hand").src = "/images/" + n + ".svg";
   }
 
+  // Listeners for Starting Game
   startnewBtn.onclick = function () {
     if (usernameInput.value != "") {
       gameConn = new GameConnection("TTT", usernameInput.value, null);
@@ -96,9 +42,9 @@ function ticTacToe() {
         "</h2> <h4> Waiting for the other player to join...</h4>";
       joingameBtn.style.display = "none";
       startnewBtn.style.display = "none";
+      gameState.turn = "player1";
     }
   };
-
   enteroldBtn.onclick = function () {
     const username = usernameInput.value;
     if (usernameInput.value != "" && gameroomInput.value != "") {
@@ -110,22 +56,78 @@ function ticTacToe() {
       gameConn.onData(onGetData);
       gameConn.sendData({ username });
       modal.style.display = "none";
+      gameState.turn = "player2";
     }
   };
-
   joingameBtn.onclick = function () {
     startnewBtn.style.display = "none";
     gameroomInput.style.display = "block";
     joingameBtn.style.display = "none";
     enteroldBtn.style.display = "block";
   };
+
+  // Listener for controls
   let controls = document.getElementsByClassName("control");
   for (var i = 1; i < 7; i++)
     (function (i) {
       controls[i - 1].onclick = function () {
-        document.getElementById("player-hand").src = "/images/" + i + ".svg";
-        gameConn.sendData(JSON.stringify(i));
-      };
+      //   document.getElementById("player-hand").src = "/images/" + i + ".svg";
+      //   gameConn.sendData(JSON.stringify(i));
+      gameState.makeMove(i);
+    };
       console.log(i);
     })(i);
+
+  // const heads = document.querySelector("#headsCount");
+  // const tails = document.querySelector("#tailsCount");
+
+  // let headsCount = 0;
+  // let tailsCount = 0;
+
+  // function deferFn(callback, ms) {
+  //   setTimeout(callback, ms);
+  // }
+
+  // function processResult(result) {
+  //   if (result === "heads") {
+  //     headsCount++;
+  //     heads.innerText = headsCount;
+  //   } else {
+  //     tailsCount++;
+  //     tails.innerText = tailsCount;
+  //   }
+  //   status.innerText = result.toUpperCase();
+  // }
+
+  // function flipCoin() {
+  //   coin.setAttribute("class", "");
+  //   const random = Math.random();
+  //   const result = random < 0.5 ? "heads" : "tails";
+  //   deferFn(function () {
+  //     coin.setAttribute("class", "animate-" + result);
+  //     deferFn(processResult.bind(null, result), 2900);
+  //   }, 100);
+  // }
+
+  // button.addEventListener("click", flipCoin);
+  // // Get the modal
+  // var modal1 = document.getElementById("myModal1");
+  // var btn = document.getElementById("myBtnn");
+  // var span = document.getElementsByClassName("close1")[0];
+  // btn.onclick = function () {
+  //   modal1.style.display = "block";
+  // };
+
+  // span.onclick = function () {
+  //   modal1.style.display = "none";
+  // };
+  // window.onclick = function (event) {
+  //   if (event.target == modal1) {
+  //     modal1.style.display = "none";
+  //   }
+  // };
+
+  // const coin = document.querySelector("#coin");
+  // const button = document.querySelector("#flip");
+  // const status = document.querySelector("#status");
 }
